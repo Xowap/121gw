070aa79db73073f2929795f17f143446df855177
Rémy Sanchez <remy.sanchez@hyperthese.net>
2023-06-24 18:26:11 +0200

    Better inputs parsing

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const core = require("@actions/core");
+const github = require("@actions/github");
 const deploy = require("./deploy");
 
 async function run() {
+    let branch = core.getInput("branch");
+
+    if (!branch) {
+        branch = github.context.ref.replace("refs/heads/", "");
+    }
+
     try {
         await deploy({
             endpoint: core.getInput("endpoint"),
             token: core.getInput("token"),
             file: core.getInput("file"),
-            timeout: core.getInput("timeout"),
-            branch: core.getInput("branch"),
+            timeout: parseInt(core.getInput("timeout"), 10),
+            branch: branch,
         });
     } catch (error) {
         core.setFailed(error.message);

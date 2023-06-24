bf34ddb226cb04784fe84421a3339e3628edf265
Rémy Sanchez <remy.sanchez@hyperthese.net>
2023-06-24 15:41:44 +0200

    CLI mode + fixes

---
+++ cli.js
@@ -0,0 +1,51 @@
+const deploy = require("./deploy");
+
+const argv = require("yargs/yargs")(process.argv.slice(2))
+    .option("branch", {
+        alias: "b",
+        description: "Branch to deploy",
+        type: "string",
+    })
+    .option("file", {
+        alias: "f",
+        description: "Fluxfile to use",
+        type: "string",
+    })
+    .option("timeout", {
+        alias: "t",
+        description: "Timeout in seconds",
+        default: 1800, // 30 minutes
+        type: "number",
+    })
+    .help()
+    .alias("help", "h").argv;
+
+const FLUX_ENDPOINT = process.env.FLUX_ENDPOINT;
+const FLUX_TOKEN = process.env.FLUX_TOKEN;
+
+async function main() {
+    if (!FLUX_ENDPOINT) {
+        console.error("FLUX_ENDPOINT not set");
+        process.exit(1);
+    }
+
+    if (!FLUX_TOKEN) {
+        console.error("FLUX_TOKEN not set");
+        process.exit(1);
+    }
+
+    try {
+        await deploy({
+            endpoint: FLUX_ENDPOINT,
+            token: FLUX_TOKEN,
+            file: argv.file,
+            timeout: argv.timeout,
+            branch: argv.branch,
+        });
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+}
+
+main().then();
